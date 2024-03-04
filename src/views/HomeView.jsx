1ec4ef6e759ce0b67f8a67c84802e649c231d205
leesangjun <hubls@naver.com>
2024-03-04 22:37:44 +0900

    add: 기본 기능 추가

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const HomeView = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default HomeView
