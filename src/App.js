9fac680c410cd80e1e32be4288365cd4301ca39c
leesangjun <hubls@naver.com>
2024-03-05 22:32:13 +0900

    add: 기본 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomeView from "./views/HomeView";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-      </Routes>
+      <Navbar/>
     </BrowserRouter>
   )
 }
